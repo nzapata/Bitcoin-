73a3a756ed436151aae17ff8f28ec8a849923cff
Naze Zapata <nathaniel.zapata@gmail.com>
2019-01-23 09:24:06 -0500

    Updated Btc.js

--- controllers/BTC.js
+++ controllers/BTC.js
@@ -46,22 +46,6 @@ function initAccountSnapshot() {
   });
 }
 
-// Generate Random Guid
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-
-// Time
-const serverTime = () => (
-  {
-    current: () => moment().format('YYYY-MM-DD HH:mm:ss'),
-    future: () => moment().add(10, 'm').format('YYYY-MM-DD HH:mm:ss')
-  }
-);
 
 // Get last block
 const getLaskBlock = (callback) => {
